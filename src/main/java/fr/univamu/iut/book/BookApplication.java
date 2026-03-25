1de949705567fa1cd440e964c3dec9ba612ba828
Loan <loan.allard@etu.univ-amu.fr>
2026-03-25 17:04:30 +0100

    Tout le code jusqu'à 1.6

---
+++ src/main/java/fr/univamu/iut/book/BookApplication.java
@@ -0,0 +1,9 @@
+package fr.univamu.iut.book;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class BookApplication extends Application {
+
+}
